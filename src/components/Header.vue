5496d8d0090c3b0492715694a5ac385a2c7f940b
swordman-sm <aevlp@aevlp.vip>
2021-11-30 00:44:01 +0800

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="header">
+    <!--  折叠按钮  -->
+    <div class="collapse-btn" @click="collapseChange">
+      <i v-if="!collapse" class="el-icon-s-fold"></i>
+      <i v-else class="el-icon-s-unfold"></i>
+    </div>
+    <div class="logo">监控系统</div>
+  </div>
+</template>
+
+<script>
+import useStore from 'vuex/dist/vuex.mjs'
+import {computed, onMounted} from 'vue'
+import {useRouter} from 'vue-router'
+
+export default {
+  name: 'Header',
+  setup () {
+    const username = localStorage.getItem('ms_username')
+    const message = 2
+
+    const store = useStore()
+    const collapse = computed(() => store.state.collapse)
+
+    // 侧边栏折叠
+    const collapseChange = () => {
+      store.commit('handleCollapse', !collapse.value)
+    }
+
+    // 加载完毕就执行的
+    onMounted(() => {
+      if (document.body.clientWidth < 1500) {
+        collapseChange()
+      }
+    })
+
+    // 用户名下拉菜单选择事件
+    const router = useRouter()
+    const handleCommand = (command) => {
+      if (command == 'loginout') {
+        localStorage.removeItem('ms_username')
+        router.push('/login')
+      }
+      if (command == 'user') {
+        router.push('/user')
+      }
+    }
+
+    return {
+      username,
+      message,
+      collapse,
+      collapseChange,
+      handleCommand
+    }
+  }
+}
+</script>
+
+<style scoped>
+.header {
+
+}
+</style>
